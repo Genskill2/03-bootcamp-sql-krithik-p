0cce2941bdfd9ad2ad09f4798207ce639b42ef40
krithik-p <krithik.p.babu@gmail.com>
2021-06-19 17:19:28 +0530

    try 2

--- query3.sql
+++ query3.sql
@@ -1,2 +1,2 @@
-SELECT books.title FROM books,subjects,books_subjects WHERE(books.id = books_subjects.book AND subjects.id = books_subjects.subject AND subjects.name IN('Technology','Politics'));
+SELECT books.title FROM books,subjects,books_subjects WHERE(books.id = books_subjects.book AND subjects.id = books_subjects.subject AND subjects.name ='Technology' OR 'Politics');
 
