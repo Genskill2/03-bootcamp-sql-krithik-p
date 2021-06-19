bedeb7b4392bafecb7ecaaa8376b3ee295c37c94
krithik-p <krithik.p.babu@gmail.com>
2021-06-19 20:53:13 +0530

    trying

--- query4.sql
+++ query4.sql
@@ -1,1 +1,1 @@
-SELECT subjects.name FROM books,subjects,books_subjects WHERE(books.id = books_subjects.book AND subjects.id = books_subjects.subject AND books.title = 'Atomic Habits');
+SELECT subjects.name FROM subjects,books,books_subjects WHERE(books.id = books_subjects.book AND subjects.id = books_subjects.subject AND books.title = 'Atomic Habits');
