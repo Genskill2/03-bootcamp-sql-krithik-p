e72835eb47e3d36e7c94f64ffc85a260f8d8b146
krithik-p <krithik.p.babu@gmail.com>
2021-06-20 11:23:30 +0530

    changed int to text for some

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 --PARMA foreign_keys = ON;
 
 CREATE TABLE publisher(id INTEGER PRIMARY KEY, name TEXT,country TEXT);
-CREATE TABLE books(id INTEGER PRIMARY KEY, title TEXT,publisher INTEGER,FOREIGN KEY(publisher)REFERENCES publisher(id));
+CREATE TABLE books(id INTEGER PRIMARY KEY, title TEXT,publisher TEXT,FOREIGN KEY(publisher)REFERENCES publisher(id));
 CREATE TABLE subjects(id INTEGER , name TEXT);
-CREATE TABLE books_subjects(book INTEGER,subject INTEGER,FOREIGN KEY(book)REFERENCES books(id),FOREIGN KEY(subject)REFERENCES subjects(id));
+CREATE TABLE books_subjects(book TEXT,subject TEXT,FOREIGN KEY(book)REFERENCES books(id),FOREIGN KEY(subject)REFERENCES subjects(id));
 
